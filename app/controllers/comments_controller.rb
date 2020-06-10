905d781f35de60095b090662dd827d05d015450b
Janis Rancourt <janis.rancourt@gmail.com>
2020-06-10 14:35:20 -0400

    fix comments controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,10 +1,13 @@
 # frozen_string_literal: true
 
 class CommentsController < ApplicationController
+  before_action :authorize_request
+
   # POST /posts
   def create
     @comment = Comment.new(comment_params)
     @comment.user = @current_user
+    @comment.post = Post.find(params[:post_id])
 
     if @comment.save
       render json: @comment, status: :created
