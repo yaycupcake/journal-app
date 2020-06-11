c624899775c39106033bacef3070d634f98ac9b4
Janis Rancourt <janis.rancourt@gmail.com>
2020-06-11 09:06:49 -0400

    adding some front end routing

--- client/src/components/shared/Nav.jsx
+++ client/src/components/shared/Nav.jsx
@@ -12,9 +12,31 @@ export default function Nav(props) {
        :
        'Guest'
      }</li>
-     <li>placeholder</li>
-     <li>placeholder</li>
-     <li>placeholder</li>
+     <li>{
+      props.currentUser
+       ?
+       <Link to='/my-posts'>My Posts</Link>
+       :
+       <Link to='/login'>Login</Link>
+     }</li>
+     <li>{
+      props.currentUser
+       ?
+       <Link>Logout</Link>
+       // gotta fix this line later to actually logout
+       :
+       <Link to='/sign-up'>Sign Up</Link>
+     }</li>
+     <li>{
+      props.currentUser
+      &&
+      <Link to='/preferences'>Preferences</Link>
+     }</li>
+     <li>{
+      props.currentUser
+      &&
+      <Link to='/new-post'>New Post</Link>
+     }</li>
     </ul>
    </nav>
   </aside>
