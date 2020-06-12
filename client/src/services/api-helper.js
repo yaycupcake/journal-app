7faaa23ed0f54c8c3030145d7a3c2f967ca655f1
Janis Rancourt <janis.rancourt@gmail.com>
2020-06-12 15:32:33 -0400

    updating api helper for production

--- client/src/services/api-helper.js
+++ client/src/services/api-helper.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const baseUrl = 'http://localhost:3000'
+const baseUrl = process.env.NODE_ENV === 'production' ? 'https://ycc-journal-app' : 'http://localhost:3000'
+
 
 const api = axios.create({
  baseURL: baseUrl
